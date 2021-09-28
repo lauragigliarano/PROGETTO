b4f66a8d320960ccdce7e2a3aba53faa2e057ad1
laura.gigliarano <laura.gigliarano@enet.it>
2021-09-28 12:30:18 +0200

    fix

--- PROGETTO/Models/Cliente.cs
+++ PROGETTO/Models/Cliente.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PROGETTO.Models
 {
     public class Cliente
     {
         public int ClienteID { get; set; }
+        [Display(Name = "Ragione Sociale")]
         public string RagioneSociale { get; set; }
         public string Telefono { get; set; }
         public string Mail { get; set; }
--- PROGETTO/Models/Commessa.cs
+++ PROGETTO/Models/Commessa.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PROGETTO.Models
 {
     public class Commessa
     {
+        [Display(Name = "Commessa")]
         public int CommessaID { get; set; }
         public string Descrizione { get; set; }
         public int ClienteID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Data Inizio")]
         public DateTime DataInizio { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Data Fine")]
         public DateTime DataFine { get; set; }
 
         //public virtual Cliente Cliente { get; set; }
