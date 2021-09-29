b276b302aa6fc07c04d735c1248dd24dde41ce75
laura.gigliarano <laura.gigliarano@enet.it>
2021-09-29 08:49:39 +0200

    fix

--- PROGETTO/Models/Commessa.cs
+++ PROGETTO/Models/Commessa.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PROGETTO.Models
 {
     public class Commessa
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Display(Name = "Commessa")]
         public int CommessaID { get; set; }
         public string Descrizione { get; set; }
+        [Display(Name = "Cliente")]
         public int ClienteID { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
@@ -18,7 +21,8 @@ namespace PROGETTO.Models
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Data Fine")]
         public DateTime DataFine { get; set; }
-
+        public decimal Importo { get; set; }
         //public virtual Cliente Cliente { get; set; }
+
     }
 }
